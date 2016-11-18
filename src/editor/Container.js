251aea17783e2384f8f1f9764b78c88a28b4dcc3
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-11-18 12:24:00 +0100

    Editor wrapper

--- src/editor/Container.js
+++ src/editor/Container.js
@@ -1,8 +1,20 @@
+import React from 'react';
 import {connect} from 'react-redux';
 import {Editor} from 'draft-js';
 
+import './index.less';
+
 import {getEditorState} from './selectors';
 import {setEditorState} from './actions';
+
+const CustomEditor = (props) => {
+    let editor;
+    return (
+        <div id="editor" onClick={() => editor.focus()}>
+            <Editor {...props} ref={(component) => { editor = component; }} />
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => ({
     editorState: getEditorState(state),
@@ -12,4 +24,4 @@ const mapDispatchToProps = (dispatch) => ({
     onChange: (editorState) => dispatch(setEditorState(editorState)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Editor);
+export default connect(mapStateToProps, mapDispatchToProps)(CustomEditor);
