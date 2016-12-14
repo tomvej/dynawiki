5c5353548d573a0934263ab640645f9f27567347
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2016-12-14 17:02:42 +0100

    ContentStateModifier refactoring + inline style

--- src/editor/util/ContentStateModifier.js
+++ src/editor/util/ContentStateModifier.js
@@ -1,14 +1,24 @@
 import {Record} from 'immutable';
+import {Modifier, SelectionState} from 'draft-js';
 
-class ContentStateModifier extends Record({target: null}) {
+class ContentStateModifier extends Record({contentState: null}) {
 
-    setBlockType(blockKey, newType) {
-        return this.setIn(['target', 'blockMap', blockKey, 'type'], newType);
+    setBlockType(blockKey, blockType) {
+        const selection = SelectionState.createEmpty(blockKey);
+        return Modifier.setBlockType(this.contentState, selection, blockType);
+    }
+
+    setInlineStyle(blockKey, start, end, inlineStyle) {
+        const selection = SelectionState.createEmpty(blockKey).merge({
+            anchorOffset: start,
+            focusOffset: end,
+        });
+        return Modifier.applyInlineStyle(this.contentState, selection, inlineStyle);
     }
 
     result() {
-        return this.target;
+        return this.contentState;
     }
 }
 
-export default (contentStyle) => new ContentStateModifier({target: contentStyle});
+export default (contentState) => new ContentStateModifier({contentState});
