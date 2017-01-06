5d310dc85fb3d5392adcb8b067c55a2bc706775d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2017-01-06 11:55:38 +0100

    Extraneous file

--- src/editor/actions.js
+++
@@ -1,8 +0,0 @@
-import {NAME} from './constants';
-
-export const SET_EDITOR_STATE = `${NAME}/SET_EDITOR_STATE`;
-
-export const setEditorState = (editorState) => ({
-    type: SET_EDITOR_STATE,
-    editorState,
-});
